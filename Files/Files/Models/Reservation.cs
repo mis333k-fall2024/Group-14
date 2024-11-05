0436ec9bd40d24fa9557058226f889c527d1b542
yymariana <mariana.alvarez@utexas.edu>
2024-11-05 17:52:53 -0600

    reservation

---
+++ Files/Files/Models/Reservation.cs
@@ -0,0 +1,61 @@
+﻿
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace FA24_Group14.Models
+{
+    public class Reservation
+    {
+        public const Decimal Tax = .07m;
+
+        private const int START_CONFIRMATION_NUMBER = 21901;
+
+        private static int _nextConfirmationNumber = START_CONFIRMATION_NUMBER;
+
+        [Key]
+        [Display(Name = "Reservation ID")]
+        public Int32 ReservationID { get; set; }
+
+        [Display(Name = "Check In")]
+        public DateTime CheckIn { get; set; }
+
+        [Display(Name = "Check Out")]
+        public DateTime CheckOut { get; set; }
+
+        [Display(Name = "Number of Guests")]
+        public Int32 NumOfGuests { get; set; }
+
+        [Display(Name = "Weekday Price")]
+        [DisplayFormat(DataFormatString = "{0:c}")]
+        public Decimal WeekdayPrice { get; set; }
+
+        [Display(Name = "Weekend Price")]
+        [DisplayFormat(DataFormatString = "{0:c}")]
+        public Decimal WeekendPrice { get; set; }
+
+        [Display(Name = "Cleaning Fee")]
+        [DisplayFormat(DataFormatString = "{0:c}")]
+        public Decimal CleaningFee { get; set; }
+
+        [Display(Name = "Discount Rate")]
+        public Decimal DiscountRate { get; set; }
+
+        [Display(Name = "Reservation Status")]
+        public Boolean ReservationStatus { get; set; }
+
+        [Display(Name = "Confirmation Number")]
+        public Int32 ConfirmationNumber { get; private set; }
+
+        // Constructor to set the ConfirmationNumber
+        public Reservation()
+        {
+            ConfirmationNumber = GenerateConfirmationNumber();
+        }
+
+        // Method to generate a confirmation number
+        private static int GenerateConfirmationNumber()
+        {
+            return _nextConfirmationNumber++;
+        }
+    }
+}
--- Files/Files/obj/Debug/net8.0/Files.AssemblyInfo.cs
+++ Files/Files/obj/Debug/net8.0/Files.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Files")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ee820c9dae33ecc0efe5b686e1981d1be7da83f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Files")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Files")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
