fe5ff95cfc832fd20cf36fee15b25acc1e671f92
sophier2003 <sophieisabeller@gmail.com>
2024-11-14 12:16:10 -0600

    Review

--- Files/Files/obj/Debug/net8.0/Files.AssemblyInfo.cs
+++ Files/Files/obj/Debug/net8.0/Files.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Files")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d66323514230d63e6806b539b2c9d9c9eb3514b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9545a48173b11abba3a96a5445e1e73a0c09cec")]
 [assembly: System.Reflection.AssemblyProductAttribute("Files")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Files")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
