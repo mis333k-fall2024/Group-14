d6ea79411560adef757f1b25d490603c98f0fd9e
sonadele <onadelestar@gmail.com>
2024-11-19 17:29:13 -0600

    Added Folders (controllers, views, shared)

--- Files/Files/obj/Debug/net8.0/Files.AssemblyInfo.cs
+++ Files/Files/obj/Debug/net8.0/Files.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Files")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c87532307d8e91c8076f7d0ab4d0b05cff7ec44a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2028a7a1550f8f1c5d978803b0468dd5ddd25009")]
 [assembly: System.Reflection.AssemblyProductAttribute("Files")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Files")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
