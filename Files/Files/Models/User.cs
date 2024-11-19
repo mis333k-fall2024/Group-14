2028a7a1550f8f1c5d978803b0468dd5ddd25009
Jordan Burks <jburks2003@utexas.edu>
2024-11-19 17:07:39 -0600

    Update User.cs w/ Error Messages

--- Files/Files/Models/User.cs
+++ Files/Files/Models/User.cs
@@ -6,14 +6,21 @@ public class User
     [Key]
     public int UserId { get; set; } // Primary Key
 
-    [Required]
-    [EmailAddress]
+    [Required(ErrorMessage = "Email is required.")]
+    [EmailAddress(ErrorMessage = "Invalid email format.")]
     public string Email { get; set; }
 
-    [Required]
+    [Required(ErrorMessage = "Password is required.")]
+    [MinLength(8, ErrorMessage = "Password must be at least 8 characters long.")]
+    [MaxLength(100, ErrorMessage = "Password cannot exceed 100 characters.")]
     public string Password { get; set; }
 
+    [Required(ErrorMessage = "First name is required.")]
+    [MaxLength(50, ErrorMessage = "First name cannot exceed 50 characters.")]
     public string FirstName { get; set; }
+
+    [Required(ErrorMessage = "Last name is required.")]
+    [MaxLength(50, ErrorMessage = "Last name cannot exceed 50 characters.")]
     public string LastName { get; set; }
 
     // Navigation Properties
@@ -21,3 +28,4 @@ public class User
     public virtual ICollection<Property> Properties { get; set; } // One-to-Many with Properties (Host role)
     public virtual ICollection<Review> Reviews { get; set; } // One-to-Many with Reviews
 }
+
