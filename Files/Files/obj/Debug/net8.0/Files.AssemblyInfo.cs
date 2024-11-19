8431c71ba73282992f27c3513567fb0547898fe9
sonadele <onadelestar@gmail.com>
2024-11-19 17:33:02 -0600

    Controller file + View file

--- Files/Files/obj/Debug/net8.0/Files.AssemblyInfo.cs
+++ Files/Files/obj/Debug/net8.0/Files.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Files")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2028a7a1550f8f1c5d978803b0468dd5ddd25009")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6ea79411560adef757f1b25d490603c98f0fd9e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Files")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Files")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
