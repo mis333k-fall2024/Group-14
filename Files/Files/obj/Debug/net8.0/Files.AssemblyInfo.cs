3024cea87f71e6e7db888aa7b12d2f589c1c4d05
yymariana <mariana.alvarez@utexas.edu>
2024-11-20 17:56:52 -0600

    final model updates part 1

--- Files/Files/obj/Debug/net8.0/Files.AssemblyInfo.cs
+++ Files/Files/obj/Debug/net8.0/Files.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Files")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bef17bddffc6735989923cb30ee47ffd942c4cb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16d66404a8ecb02797a65e9049df78eec3ef044a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Files")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Files")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
