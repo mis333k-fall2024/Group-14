faee6fd6cf04dc930fd361f0d24e0d87422cc60d
sonadele <onadelestar@gmail.com>
2024-11-20 13:30:00 -0600

    Appuser change

--- Files/Files/obj/Debug/net8.0/Files.AssemblyInfo.cs
+++ Files/Files/obj/Debug/net8.0/Files.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Files")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85cbfa9d005498ba2fb57d8922ff942d81249e73")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bef17bddffc6735989923cb30ee47ffd942c4cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Files")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Files")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
