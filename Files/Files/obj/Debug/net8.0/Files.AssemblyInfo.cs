7b496d4cf601476d1dc7797daa08daa2b571f2b7
sonadele <onadelestar@gmail.com>
2024-11-21 10:45:15 -0600

    core

--- Files/Files/obj/Debug/net8.0/Files.AssemblyInfo.cs
+++ Files/Files/obj/Debug/net8.0/Files.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Files")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd31cfec806b175b52bc68a9dce3b33e1384468d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+add9e02747c478242af4bf4060f249d5c7361486")]
 [assembly: System.Reflection.AssemblyProductAttribute("Files")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Files")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
