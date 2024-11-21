9a9425113823d5ed5fb7f3cb98bd1b9e01d08cc2
sophier2003 <sophieisabeller@gmail.com>
2024-11-21 11:43:30 -0600

    idk

--- Files/Files/obj/Debug/net8.0/Files.AssemblyInfo.cs
+++ Files/Files/obj/Debug/net8.0/Files.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Files")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd31cfec806b175b52bc68a9dce3b33e1384468d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34a6aa5f85d1b863cf81fece67997393229a9353")]
 [assembly: System.Reflection.AssemblyProductAttribute("Files")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Files")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
