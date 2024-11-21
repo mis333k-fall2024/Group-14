5f71414de6494c6e38fa551022586c7336660024
sonadele <onadelestar@gmail.com>
2024-11-21 12:16:15 -0600

    ???

--- Files/Files/obj/Debug/net8.0/Files.AssemblyInfo.cs
+++ Files/Files/obj/Debug/net8.0/Files.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Files")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8ffb5db6377a1b364135da47a206664df84a3d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9025663fa119e0ac56806db18fc4ab74df22bf8b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Files")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Files")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
