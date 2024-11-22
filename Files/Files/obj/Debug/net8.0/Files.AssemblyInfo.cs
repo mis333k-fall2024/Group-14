5657010b214410de3df1b9b8b9df8fc252b2dd11
yymariana <mariana.alvarez@utexas.edu>
2024-11-22 12:18:21 -0600

    new seed controller

--- Files/Files/obj/Debug/net8.0/Files.AssemblyInfo.cs
+++ Files/Files/obj/Debug/net8.0/Files.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Files")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+454f4ce3cbf2d85d879c98b585125478b1e8f00d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f9b5be9e9d42276100a260ddba3ddb75e6bc51e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Files")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Files")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
