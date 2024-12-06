675d41d333517dc012fc3155cb48f1753b93f543
yymariana <mariana.alvarez@utexas.edu>
2024-12-06 17:14:08 -0600

    ?

--- Files/Files/obj/Debug/net8.0/Files.AssemblyInfo.cs
+++ Files/Files/obj/Debug/net8.0/Files.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Files")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63310315205b9220b78e5ef1c28c238ea855f926")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4348edab71abc30992b9fb3e4ef1089813792f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Files")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Files")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
