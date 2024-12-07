128e7737c1f2f4db1c860c9cd2e87b095f69dc1b
jburks2003 <jburks2003@utexas.edu>
2024-12-07 16:35:58 -0600

    w

--- Files/Files/obj/Debug/net8.0/Files.AssemblyInfo.cs
+++ Files/Files/obj/Debug/net8.0/Files.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Files")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06a716fd6f3ef8a5d7d281143d34da272e4c28c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+792d603e7afe60d9ef3097fd2603c0df106c9c3a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Files")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Files")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
