ef977323eb1db22789692938dbd239ac827d11ea
Jordan Burks <jburks2003@utexas.edu>
2024-12-07 19:06:16 -0600

    Update ModifyProfileViewModel.cs

--- Files/Files/Models/ViewModels/ModifyProfileViewModel.cs
+++ Files/Files/Models/ViewModels/ModifyProfileViewModel.cs
@@ -4,34 +4,13 @@ using System.ComponentModel.DataAnnotations;
 namespace Files
 {
     public class ModifyProfileViewModel
-    {
-        [Required(ErrorMessage = "First name is required.")]
-        [Display(Name = "First Name")]
-        public string FirstName { get; set; }
+{
+    public string UserId { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Address { get; set; }
+    public string PhoneNumber { get; set; }
+    public DateTime DOB { get; set; }
+}
 
-        [Required(ErrorMessage = "Last name is required.")]
-        [Display(Name = "Last Name")]
-        public string LastName { get; set; }
-
-        [Required(ErrorMessage = "Phone number is required")]
-        [Phone]
-        [Display(Name = "Phone Number")]
-        public string PhoneNumber { get; set; }
-
-        [Required(ErrorMessage = "Address is required.")]
-        [Display(Name = "Address")]
-        public string Address { get; set; }
-
-        [Required(ErrorMessage = "Date of Birth is required.")]
-        [Display(Name = "Date of Birth")]
-        public DateTime DOB { get; set; }
-
-        [DataType(DataType.Password)]
-        [Display(Name = "New Password")]
-        public string NewPassword { get; set; }
-
-        [DataType(DataType.Password)]
-        [Display(Name = "Current Password")]
-        public string OldPassword { get; set; }
-    }
 }
