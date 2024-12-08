c0da426220712d6acb5331b9b90b1ed1f7c989e5
Jordan Burks <jburks2003@utexas.edu>
2024-12-07 19:13:25 -0600

    Update HireAdminViewModel.cs

--- Files/Files/Models/ViewModels/HireAdminViewModel.cs
+++ Files/Files/Models/ViewModels/HireAdminViewModel.cs
@@ -3,13 +3,50 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Files
 {
-  public class HireAdminViewModel
-{
-    public string Email { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string Address { get; set; }
-    public string PhoneNumber { get; set; }
-    public DateTime DOB { get; set; }
-    public string Password { get; set; }
+    public class HireAdminViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "Last name cannot be longer than 50 characters.")]
+        public string LastName { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "Address cannot be longer than 100 characters.")]
+        public string Address { get; set; }
+
+        [Required]
+        [Phone]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [AgeValidation(ErrorMessage = "Admin must be at least 18 years old.")]
+        public DateTime DOB { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+
+    // Custom validation attribute to check if the user is at least 18 years old
+    public class AgeValidation : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            var dob = (DateTime)value;
+            var age = DateTime.Now.Year - dob.Year;
+
+            if (DateTime.Now.DayOfYear < dob.DayOfYear)
+                age--;
+
+            return age >= 18;
+        }
+    }
 }
