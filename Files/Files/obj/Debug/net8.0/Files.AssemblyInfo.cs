9e142958e5a5f8ce9d24cdbcacb58d4a9bcec929
yymariana <mariana.alvarez@utexas.edu>
2024-12-08 18:31:28 -0600

    first round reseed

--- Files/Files/obj/Debug/net8.0/Files.AssemblyInfo.cs
+++ Files/Files/obj/Debug/net8.0/Files.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Files")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4a58660f5adba7ba7a9a396386de8e3f90b75ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+301720a8f9704530c0c05fb5eb15ba0ded90636e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Files")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Files")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
