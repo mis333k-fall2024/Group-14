301720a8f9704530c0c05fb5eb15ba0ded90636e
sophier2003 <sophieisabeller@gmail.com>
2024-12-08 18:21:05 -0600

    migration

---
+++ Files/Files/Migrations/20241209001712_Setup5.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Files.Migrations
+{
+    /// <inheritdoc />
+    public partial class Setup5 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "Reservations",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "DiscountAmount",
+                table: "Reservations",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<string>(
+                name: "State",
+                table: "Reservations",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "TransactionId",
+                table: "Reservations",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Transactions",
+                columns: table => new
+                {
+                    TransactionId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ConfirmationNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TransactionDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Subtotal = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Tax = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    GrandTotal = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transactions", x => x.TransactionId);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reservations_TransactionId",
+                table: "Reservations",
+                column: "TransactionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_Email",
+                table: "AspNetUsers",
+                column: "Email",
+                unique: true,
+                filter: "[Email] IS NOT NULL");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reservations_Transactions_TransactionId",
+                table: "Reservations",
+                column: "TransactionId",
+                principalTable: "Transactions",
+                principalColumn: "TransactionId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reservations_Transactions_TransactionId",
+                table: "Reservations");
+
+            migrationBuilder.DropTable(
+                name: "Transactions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Reservations_TransactionId",
+                table: "Reservations");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_Email",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "Reservations");
+
+            migrationBuilder.DropColumn(
+                name: "DiscountAmount",
+                table: "Reservations");
+
+            migrationBuilder.DropColumn(
+                name: "State",
+                table: "Reservations");
+
+            migrationBuilder.DropColumn(
+                name: "TransactionId",
+                table: "Reservations");
+        }
+    }
+}
--- Files/Files/obj/Debug/net8.0/Files.AssemblyInfo.cs
+++ Files/Files/obj/Debug/net8.0/Files.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Files")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a38c61c5ff721e808c166ae668092d6b80b62c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4a58660f5adba7ba7a9a396386de8e3f90b75ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("Files")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Files")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
